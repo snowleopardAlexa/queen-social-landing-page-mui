cb322b530b294478254e2fce8b4ffd2a5067e5c4
snowleopardAlexa <alexaslomski@gmail.com>
2022-01-30 13:38:20 -0500

    add photo gallery

--- src/components/RightBar.jsx
+++ src/components/RightBar.jsx
@@ -1,8 +1,14 @@
-import { makeStyles, Container, Typography } from "@material-ui/core";
+import { makeStyles, Container, Typography, ImageList, ImageListItem } from "@material-ui/core";
 import { AvatarGroup, Avatar } from "@material-ui/lab";
 import john from ".././img/john.png";
 import brian from ".././img/brian.jpeg";
 import roger from ".././img/roger.png";
+import yellow from ".././img/yellow.jpeg";
+import london from ".././img/london.jpeg";
+import women from ".././img/women.png";
+import tall from ".././img/tall.jpeg";
+import white from ".././img/white.jpeg";
+import butterflies from ".././img/butterflies.jpeg";
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -31,6 +37,27 @@ function RightBar() {
        <Avatar alt="Roger Tayler" src={roger} />
        <Avatar alt="Roger Tayler" src={roger} />
      </AvatarGroup>
+     <Typography className={classes.title} gutterBottom>Gallery</Typography>
+     <ImageList rowHeight={100} style={{ marginBottom: 20 }} cols={2}>
+        <ImageListItem>
+          <img src={yellow} alt="Yellow Freddie" />
+        </ImageListItem>
+        <ImageListItem>
+          <img src={london} alt="London" />
+        </ImageListItem>
+        <ImageListItem>
+          <img src={women} alt="Queen as Women" />
+        </ImageListItem>
+        <ImageListItem>
+          <img src={tall} alt="Tall building" />
+        </ImageListItem>
+        <ImageListItem>
+          <img src={white} alt="White Outfits" />
+        </ImageListItem>
+        <ImageListItem>
+          <img src={butterflies} alt="With butterflies" />
+        </ImageListItem>
+      </ImageList>
   </Container> 
   );
 }
