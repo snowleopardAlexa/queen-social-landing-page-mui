5bbb08039633a31c89645db01b6a0714e27a3a74
snowleopardAlexa <alexaslomski@gmail.com>
2022-01-29 19:28:42 -0500

    add usestyles and button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import './App.css';
+import { Button, makeStyles } from '@material-ui/core';
+import { Person } from '@material-ui/icons';
+
+const useStyles = makeStyles({
+  button: {
+    color: "white",
+    backgroundColor: "black"
+  }
+})
 
 function App() {
+
+  const classes = useStyles()
+
   return (
     <div className="App">
-        
+        <Button
+          variant="contained"
+          size="large"
+          startIcon={<Person />}
+          className={classes.button}
+        >
+          Material UI
+        </Button>
     </div>
   );
 }
