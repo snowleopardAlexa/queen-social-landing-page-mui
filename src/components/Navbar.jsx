a0e83e310c633f056059072f01b09447d3df5081
snowleopardAlexa <alexaslomski@gmail.com>
2022-01-30 09:47:22 -0500

    add search and input and style them

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,11 @@
-import { makeStyles, AppBar, Toolbar, Typography } from "@material-ui/core";
-
+import { makeStyles, AppBar, Toolbar, Typography, InputBase, alpha } from "@material-ui/core";
+import { Search } from "@material-ui/icons";
 
 const useStyles = makeStyles((theme) => ({
+  toolbar: {
+      display: "flex",
+      justifyContent: "space-between",
+  },
   logoLg: {
       display: "none",
       [theme.breakpoints.up("sm")]: {
@@ -14,6 +18,20 @@ const useStyles = makeStyles((theme) => ({
           display: "none",
         },
      },
+     search: {
+         display: "flex",
+         alignItems: "center",
+         backgroundColor: alpha(theme.palette.common.white, 0.15),
+         '&:hover': {
+             backgroundColor: alpha(theme.palette.common.white, 0.25),
+         },
+         borderRadius: theme.shape.borderRaius,
+         width: "50%", 
+     },
+     input: {
+         color: "white",
+         marginLeft: theme.spacing(1),
+     }
 }));
 
 function Navbar() {
@@ -22,13 +40,21 @@ function Navbar() {
 
   return (
     <AppBar>
-        <Toolbar>
+        <Toolbar className={classes.toolbar}>
             <Typography variant="h6" className={classes.logoLg}>
                Social Landing Page
             </Typography>
             <Typography variant="h6" className={classes.logoSm}>
                Social Page
             </Typography>
+            <div className={classes.search}>
+                <Search />
+                <InputBase 
+                  placeholder="Search..."
+                  className={classes.input}
+                />
+            </div>
+
         </Toolbar>
     </AppBar>
   );
