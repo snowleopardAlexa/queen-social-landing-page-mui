0041959fb214754ad1dce45f22d5db74cc7f78cf
snowleopardAlexa <alexaslomski@gmail.com>
2022-01-30 13:24:38 -0500

    add title styles

--- src/components/RightBar.jsx
+++ src/components/RightBar.jsx
@@ -7,6 +7,11 @@ import roger from ".././img/roger.png";
 const useStyles = makeStyles((theme) => ({
   container: {
     paddingTop: theme.spacing(10),
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: 500,
+    color: "#555"
   }
 }));
 
@@ -16,10 +21,14 @@ function RightBar() {
 
   return (
   <Container className={classes.container}>
-     <Typography>Online Friends</Typography>
+     <Typography className={classes.title} gutterBottom>Online Friends</Typography>
      <AvatarGroup max={4}>
        <Avatar alt="Brian May" src={brian} />
        <Avatar alt="John Deacon" src={john} />
+       <Avatar alt="Roger Tayler" src={roger} />
+       <Avatar alt="Tina Turner" />
+       <Avatar alt="Roger Tayler" src={roger} />
+       <Avatar alt="Roger Tayler" src={roger} />
        <Avatar alt="Roger Tayler" src={roger} />
      </AvatarGroup>
   </Container> 
