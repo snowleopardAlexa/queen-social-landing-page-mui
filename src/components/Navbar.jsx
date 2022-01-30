b37d46c43fec53c63e8d64a19a3f78f580a5b5eb
snowleopardAlexa <alexaslomski@gmail.com>
2022-01-30 09:35:41 -0500

    add logo large and small with breakpoints

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,18 @@ import { makeStyles, AppBar, Toolbar, Typography } from "@material-ui/core";
 
 
 const useStyles = makeStyles((theme) => ({
-  
+  logoLg: {
+      display: "none",
+      [theme.breakpoints.up("sm")]: {
+          display: "block",
+      },
+    },
+    logoSm: {
+       display: "block",
+       [theme.breakpoints.up("sm")]: {
+          display: "none",
+        },
+     },
 }));
 
 function Navbar() {
@@ -12,8 +23,11 @@ function Navbar() {
   return (
     <AppBar>
         <Toolbar>
-            <Typography variant="h6">
+            <Typography variant="h6" className={classes.logoLg}>
                Social Landing Page
+            </Typography>
+            <Typography variant="h6" className={classes.logoSm}>
+               Social Page
             </Typography>
         </Toolbar>
     </AppBar>
